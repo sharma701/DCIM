8bbaaa5da6b465dd9fbbf75f7f1e5c973154ba22
gaurav kumar <gaurav.eeepatna@gmail.com>
2023-04-27 18:45:52 +0530

    added doProcess() method

--- Exam1.java
+++ Exam1.java
@@ -5,8 +5,14 @@ class Exam1
 	{
 		System.out.println("Exam 1 constructor");
 	}
+	Exam1(int a)
+	{
+		System.out.println("Exam 1  int parem constructor");
+	}
+	
 	public static void main(String[] args){
 		Exam1 e1=new Exam1();
+		Exam1 e1=new Exam1(10);
 	}
 
 }
